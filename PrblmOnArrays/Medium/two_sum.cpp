2a85aed126fcada246e8a87fab115a64c8b04e88
0xDev <169875752+devnchill@users.noreply.github.com>
2025-06-28 11:21:33 +0530

    feat: some more leetcode problems

--- PrblmOnArrays/Medium/two_sum.cpp
+++ PrblmOnArrays/Medium/two_sum.cpp
@@ -37,6 +37,7 @@ Constraints:
     -109 <= target <= 109
     Only one valid answer exists.
 */
+#include <algorithm>
 #include <iostream>
 #include <vector>
 class Solution {
@@ -53,13 +54,44 @@ public:
     }
     return {-1, -1};
   }
+
+  // Method 2. O(LogN) using bst.Wno't really work since vector isn't sorted and
+  // if i sort it , then index would change.
+  std::vector<int> twoSum2(std::vector<int> &nums, int target) {
+    int size = nums.size();
+    int first, second;
+    std::sort(nums.begin(), nums.end());
+    for (size_t i = 0; i < nums.size(); i++) {
+      first = nums[i];
+      second = target - first;
+      int low = i + 1;
+      int high = nums.size() - 1;
+      int mid;
+      while (low <= high) {
+        mid = (low + high) / 2;
+        if (nums[mid] == second) {
+          return {int(i), mid};
+        }
+        if (nums[mid] > second) {
+          high = mid - 1;
+        } else {
+          low = mid + 1;
+        }
+      }
+    }
+    return {-1, -1};
+  }
 };
 
 int main(int argc, char *argv[]) {
   Solution s;
   std::vector<int> nums = {3, 2, 4};
-  std::vector<int> r = s.twoSum(nums, 6);
-  for (auto it : r) {
+  // std::vector<int> r = s.twoSum(nums, 6);
+  std::vector<int> r2 = s.twoSum2(nums, 6);
+  // for (auto it : r) {
+  //   std::cout << it << std::endl;
+  // }
+  for (auto it : r2) {
     std::cout << it << std::endl;
   }
   return 0;
