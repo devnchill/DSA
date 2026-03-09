426022dd72334056806bae2eabfdcd0a64ee1163
Viena <169875752+devnchill@users.noreply.github.com>
2026-03-09 21:12:25 +0530

    haskell : solved P23

--- 99-problems/haskell/app/P23.hs
+++ 99-problems/haskell/app/P23.hs
@@ -1,6 +1,16 @@
 -- Extract a given number of randomly selected elements from a list.
-module P23 where
+module P23 (rndSelect) where
 
 import System.Random
 
-rand_select :: [a] -> Int -> [a]
+rndSelect :: [a] -> Int -> [a]
+rndSelect xs n
+  | n < 0 = error "n should be greater than 0"
+  | otherwise = gen xs n (mkStdGen 100)
+  where
+    gen _ 0 _ = []
+    gen ys k g =
+      let (i, g2) = randomR (0, length ys - 1) g
+          picked = ys !! i
+          rest = take i ys ++ drop (i + 1) ys
+       in picked : gen rest (k - 1) g2
