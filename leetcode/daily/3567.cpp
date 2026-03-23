d91126d052f4740cc1ab8ac3a634db6f21151bc3
Viena <169875752+devnchill@users.noreply.github.com>
2026-03-23 17:15:23 +0530

    getting back to grind

---
+++ leetcode/daily/3567.cpp
@@ -0,0 +1,141 @@
+// 3567. Minimum Absolute Difference in Sliding Submatrix
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given an m x n integer matrix grid and an integer k.
+//
+// For every contiguous k x k submatrix of grid, compute the minimum absolute
+// difference between any two distinct values within that submatrix.
+//
+// Return a 2D array ans of size (m - k + 1) x (n - k + 1), where ans[i][j] is
+// the minimum absolute difference in the submatrix whose top-left corner is (i,
+// j) in grid.
+//
+// Note: If all elements in the submatrix have the same value, the answer will
+// be 0.
+//
+// A submatrix (x1, y1, x2, y2) is a matrix that is formed by choosing all cells
+// matrix[x][y] where x1 <= x <= x2 and y1 <= y <= y2.
+//
+//
+// Example 1:
+//
+// Input: grid = [[1,8],[3,-2]], k = 2
+//
+// Output: [[2]]
+//
+// Explanation:
+//
+// There is only one possible k x k submatrix: [[1, 8], [3, -2]].
+// Distinct values in the submatrix are [1, 8, 3, -2].
+// The minimum absolute difference in the submatrix is |1 - 3| = 2. Thus, the
+// answer is [[2]]. Example 2:
+//
+// Input: grid = [[3,-1]], k = 1
+//
+// Output: [[0,0]]
+//
+// Explanation:
+//
+// Both k x k submatrix has only one distinct element.
+// Thus, the answer is [[0, 0]].
+// Example 3:
+//
+// Input: grid = [[1,-2,3],[2,3,5]], k = 2
+//
+// Output: [[1,2]]
+//
+// Explanation:
+//
+// There are two possible k × k submatrix:
+// Starting at (0, 0): [[1, -2], [2, 3]].
+// Distinct values in the submatrix are [1, -2, 2, 3].
+// The minimum absolute difference in the submatrix is |1 - 2| = 1.
+// Starting at (0, 1): [[-2, 3], [3, 5]].
+// Distinct values in the submatrix are [-2, 3, 5].
+// The minimum absolute difference in the submatrix is |3 - 5| = 2.
+// Thus, the answer is [[1, 2]].
+//
+//
+// Constraints:
+//
+// 1 <= m == grid.length <= 30
+// 1 <= n == grid[i].length <= 30
+// -105 <= grid[i][j] <= 105
+// 1 <= k <= min(m, n)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// METHOD 1 BRUTEFORCE
+// get all submatrix of order k X k
+// find min abs difference
+// add that to answer
+class Solution {
+  vector<vector<vector<int>>>
+  get_all_korder_matrix(int k, const vector<vector<int>> &matrix) {
+    int m = matrix.size(), n = matrix[0].size();
+    vector<vector<vector<int>>> res;
+    for (int i = 0; i + k - 1 < m; i++) {
+      for (int j = 0; j + k - 1 < n; j++) {
+        vector<vector<int>> submatrix(k, vector<int>(k));
+        for (int r = 0; r < k; r++) {
+          for (int c = 0; c < k; c++) {
+            submatrix[r][c] = matrix[i + r][j + c];
+          }
+        }
+        res.push_back(submatrix);
+      }
+    }
+    return res;
+  }
+
+  int get_min_abs_diff(vector<vector<int>> &matrix) {
+    vector<int> values;
+    for (auto &row : matrix) {
+      for (int val : row) {
+        values.push_back(val);
+      }
+    }
+    sort(values.begin(), values.end());
+    values.erase(unique(values.begin(), values.end()), values.end());
+    int min_val = INT_MAX;
+    for (size_t i = 1; i < values.size(); i++) {
+      min_val = min(abs(values[i] - values[i - 1]), min_val);
+    }
+    return min_val == INT_MAX ? 0 : min_val;
+  }
+
+public:
+  vector<vector<int>> minAbsDiff(vector<vector<int>> &grid, int k) {
+    vector<vector<vector<int>>> all_vectors = get_all_korder_matrix(k, grid);
+    int row = grid.size() - k + 1, col = grid[0].size() - k + 1;
+    vector<vector<int>> res(row, vector<int>(col));
+    vector<int> temp;
+    for (auto &submatrix : all_vectors) {
+      temp.push_back(get_min_abs_diff(submatrix));
+    }
+    int idx = 0;
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < col; j++) {
+        res[i][j] = temp[idx++];
+      }
+    }
+    return res;
+  }
+};
+
+int main() {
+  Solution s;
+  int k = 2;
+  vector<vector<int>> grid{{1, -2, 3}, {2, 3, 5}};
+  auto res = s.minAbsDiff(grid, k);
+  for (auto &row : res) {
+    for (int val : row) {
+      cout << val << " ";
+    }
+    cout << "\n";
+  }
+}
