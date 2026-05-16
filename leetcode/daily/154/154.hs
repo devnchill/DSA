b4dd1eb4aa5af90122855dbb49870d748460a1e6
Viena <169875752+devnchill@users.noreply.github.com>
2026-05-16 10:02:14 +0530

    feat: solved todays problem

---
+++ leetcode/daily/154/154.hs
@@ -0,0 +1,11 @@
+findMin :: [Int] -> Int
+findMin a = func a 0 (length a - 1)
+  where
+    func :: [Int] -> Int -> Int -> Int
+    func a low high
+      | low >= high = a !! low
+      | a !! mid > a !! high = func a (mid + 1) high
+      | a !! mid == a !! high = func a low (high - 1)
+      | otherwise = func a low mid
+      where
+        mid = low + (high - low) `div` 2
