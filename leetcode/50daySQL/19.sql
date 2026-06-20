f453459875748cf437f5e8208961601f1a761e82
Viena <169875752+devnchill@users.noreply.github.com>
2026-06-20 21:22:09 +0530

    some 50 days SQL

--- leetcode/50daySQL/19.sql
+++ leetcode/50daySQL/19.sql
@@ -73,7 +73,7 @@
 SELECT 
 query_name,
 ROUND(AVG(rating*1.0/position),2) AS quality,
-ROUND(100.0 * SUM(CASE WHEN rating<3 THEN 1 else 0 END)/COUNT(*),2) as poor_query_percentage 
+ROUND(100.0 * SUM(CASE WHEN rating<3 THEN 1 ELSE 0 END)/COUNT(*),2) as poor_query_percentage 
 FROM Queries
 GROUP BY query_name;
 
